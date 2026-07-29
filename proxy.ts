7dc155fd70715330a464d648d3a72f7af4826a46
petsamuel <petsamuel4@gmail.com>
2026-07-29 18:22:41 +0100

    fix: remove unsafe-inline and unsafe-eval from script-src CSP policy

--- proxy.ts
+++ proxy.ts
@@ -40,7 +40,7 @@ function applySecurityHeaders(response: NextResponse) {
     "Content-Security-Policy",
     [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+      "script-src 'self'",
       "style-src 'self' 'unsafe-inline'",
       "img-src 'self' data: blob: https:",
       "font-src 'self' data: https:",
